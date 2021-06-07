689a3253423c0549e135e745e35677f6576b84ed
jahfunk <xknottykidx@gmail.com>
2021-06-07 19:04:54 +0200

    cloudinary middleware

---
+++ middleware/cloudinary.js
@@ -0,0 +1,15 @@
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "urban-hobbies",
+  },
+});
+
+const upload = multer({ storage });
+//! Cloudinary Setup - END
+
+module.exports = upload;
