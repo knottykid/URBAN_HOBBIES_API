2e8ff085223d7cad19e003545242fe9c975f35f1
jahfunk <xknottykidx@gmail.com>
2021-06-10 00:58:09 +0200

    worked on handeling the post request from the form

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,37 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 const isLoggedIn = require("../middleware/isLoggedIn");
-const upload = require("../middleware/cloudinary");
+// const upload = require("../middleware/cloudinary");
+
+router.post("/profile", isLoggedIn, (req, res) => {
+  const { fullName, email, age, postalCode, gender, neighborhood } = req.body;
+
+  if (!fullName || !email || !age || !postalCode || !gender || !neighborhood) {
+    return res
+      .status(400)
+      .json({ errorMessage: "you need to complete the form before submit" });
+  }
+
+  User.create({
+    fullName,
+    email,
+    age,
+    postalCode,
+    gender,
+    neighborhood,
+    user: req.user_id,
+  }).then((goodUser) => {
+    User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $addToSet: { newValues: goodUser._id },
+      },
+      { new: true }
+    ).then((superUser) => {
+      res.json({ message: "you did it!", user: superUser });
+    });
+  });
+});
 
 router.put(`/update`, isLoggedIn, (req, res) => {
   // router.put(`/update`, (req, res) => {
